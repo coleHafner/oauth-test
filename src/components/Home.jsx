152159de2bdb85cf952ef1a28a8a78411d4ece89
Cole Hafner <colehafner@gmail.com>
2020-02-03 13:49:56 -0800

    initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+
+export const Home = () => 
+	<h1>Hello World</h1>
+
---
+++ src/index.jsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Home } from './components/Home.jsx';
+
+ReactDOM.render(<Home />, document.getElementById('root'));
